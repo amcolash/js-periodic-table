b6ea6cd1f2bf2dadee2de01d67b718a14dd05363
Andrew McOlash <amcolash@gmail.com>
2020-05-17 21:48:34 -0700

    Updates

---
+++ precache-manifest.923631c3babe3f84935ee46ead67feba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72403dfbca038818ff11119cc7324714",
+    "url": "/js-periodic-table/index.html"
+  },
+  {
+    "revision": "e72c6eb8a40257aa3ab4",
+    "url": "/js-periodic-table/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "67025ac262cfb05d6b52",
+    "url": "/js-periodic-table/static/js/2.10af828c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-periodic-table/static/js/2.10af828c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e72c6eb8a40257aa3ab4",
+    "url": "/js-periodic-table/static/js/main.f2ec38ea.chunk.js"
+  },
+  {
+    "revision": "83e468dc6f4a081cf7ff",
+    "url": "/js-periodic-table/static/js/runtime-main.ef3a3e37.js"
+  }
+]);
