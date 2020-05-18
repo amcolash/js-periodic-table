34bce5ff1a8c34221a50561e592e9dda9ae12164
Andrew McOlash <amcolash@gmail.com>
2020-05-17 22:05:56 -0700

    fix scroll on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { PeriodicTable } from './PeriodicTable';
 
 function App() {
   return (
-    <div className="App" style={{display: 'flex', width: '100vw', height: '100vh', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+    <div className="App" style={{display: 'flex', width: '100vw', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
       <h2>Periodic Table</h2>
       <PeriodicTable />
     </div>
